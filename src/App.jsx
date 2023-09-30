232a97bcb9575049cf78122e8f1a33672cc9fa22
MarcosFaggiano <90062814+MarcosFaggiano@users.noreply.github.com>
2023-09-30 18:38:20 -0300

    se agrega icono y nombre solapa nevegador

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Saludo from './components/saludo/saludo'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min'
+import NavBar from './components/NavBar'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <saludo nombre="coders" />
+     <h1>Primer Proyecto React!!</h1>
+      < NavBar nombre ="jose"/>
 
     </>
   )
--- src/components/NavBar/NavBar.jsx
+++
@@ -1,9 +0,0 @@
-const Saludo = ({ nombre }) => { //asi se crea un compoente, siempre se define con mayuscula al inicio
-  return (
-    <>
-      <button onClick={() => console.log(nombre)}>Click</button>
-    </>
-  )
-}
-
-export default Saludo
