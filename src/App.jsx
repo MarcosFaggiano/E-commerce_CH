e596365905bf5dc2e91259ad58393cfa4d8df188
MarcosFaggiano <90062814+MarcosFaggiano@users.noreply.github.com>
2023-10-01 00:29:56 -0300

    se crea archivo CartWidget .jsx y css

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,14 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min'
-import NavBar from './components/NavBar'
+import NavBar from './components/NavBar/NavBar'
 
 
 function App() {
 
   return (
     <>
-     <h1>Primer Proyecto React!!</h1>
-      < NavBar nombre ="jose"/>
-
+      <NavBar background={'transparent'} />
     </>
   )
 }
